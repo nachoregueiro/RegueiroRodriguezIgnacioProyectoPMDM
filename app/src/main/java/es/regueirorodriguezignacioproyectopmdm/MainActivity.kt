88a32503bfda9dd44337e2d9ca440d9b1f96c295
NachoRegueiro <nacho.regue@gmail.com>
2021-10-15 13:21:47 +0200

    Creado el proyecto.

--- app/src/main/java/es/regueirorodriguezignacioproyectopmdm/MainActivity.kt
+++ app/src/main/java/es/regueirorodriguezignacioproyectopmdm/MainActivity.kt
@@ -1,11 +1,26 @@
 package es.regueirorodriguezignacioproyectopmdm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
+        private lateinit var btEntrar : Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btEntrar=findViewById(R.id.btEntrar)
+
+            btEntrar.setOnClickListener{
+                val intent = Intent(this, SegundaActivity::class.java)
+                startActivity(intent)
+            }
     }
+
+
 }

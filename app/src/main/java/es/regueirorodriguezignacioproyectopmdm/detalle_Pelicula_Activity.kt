45c04f18dae5b5105b125144dbfa7a765711c49a
Nacho <nacho.regue@gmail.com>
2021-11-23 01:17:15 +0100

    Correciones visuales y visualización lista de detalles.

--- app/src/main/java/es/regueirorodriguezignacioproyectopmdm/detalle_Pelicula_Activity.kt
+++ app/src/main/java/es/regueirorodriguezignacioproyectopmdm/detalle_Pelicula_Activity.kt
@@ -2,9 +2,9 @@ package es.regueirorodriguezignacioproyectopmdm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import entities.Pelicula
-import es.regueirorodriguezignacioproyectopmdm.databinding.ActivityListaPeliculasBinding
 
 class detalle_Pelicula_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,9 +15,16 @@ class detalle_Pelicula_Activity : AppCompatActivity() {
 
         val pelicula1 = intent.extras?.get("Pelicula") as Pelicula
 
-        val tv1=findViewById<TextView>(R.id.tv1)
+        //declaración elementos
+        val tvTitulo=findViewById<TextView>(R.id.tvTitulo)
+        val tvDirector=findViewById<TextView>(R.id.tvDirector)
+        val tvActor=findViewById<TextView>(R.id.tvActor)
+        val ivFoto=findViewById<ImageView>(R.id.ivFoto)
 
-    //falta visualizar
+        //visualización elementos
+        tvTitulo.setText(pelicula1.titulo)
+        tvDirector.setText(pelicula1.director)
+        tvActor.setText(pelicula1.actor)
 
     }
 }
